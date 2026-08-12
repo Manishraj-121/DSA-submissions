69ad2d79d59d5407993a74e83c784f0f54100cf6
MANISH RAJ <159468514+Manishraj-121@users.noreply.github.com>
2026-08-12 10:17:19 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        //m1 using two nested loop then find the sum fo two value == target tc->n2
+        //m2 using two pointer(it is sorted), left=0, right=n-1 then check the sum and traverse accordingly
+        int n=numbers.size();
+        vector<int> ans;
+        int i=0;
+        int j=n-1;
+        int sum=0;
+        
+        while(i<j){
+            sum = numbers[i]+numbers[j];
+            if(sum > target){
+                j--;
+            }
+            else if(sum < target){
+                i++;
+            }
+            else if(sum == target){
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                break;
+            }
+        }
+        return ans;
+    }
+};
